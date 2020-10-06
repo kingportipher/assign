a031b8f88c929f11537bb016bb06b108bdc4daa1
unknown <COMSYS@cgl-eng004.comsysghana.com>
2020-10-06 21:36:42 +0000

    First Commit

--- mest/src/FirstComponent.js
+++ mest/src/FirstComponent.js
@@ -4,7 +4,7 @@ function FirstComponent (){
   return (
     <>
      <h2>This is the FirstComponent <a href="#">Page</a></h2>
-     <p>For more information visit <a href="https://google.com" target="_blank">Google</a></p>
+      <p>For more information visit <a href="https://google.com" target="_blank">Google</a></p>
     </>
   );
 }
--- mest/src/Header.js
+++ mest/src/Header.js
@@ -1,18 +1,23 @@
 import React from 'react'
 import ReactDOM from 'react-dom';
-import Main from './component/main';
-import App from './App';
-
+import Main from './components/main';
 
 function Header(){
- return(
-   <body>
-
-  <header id="sandy"></header>
-
-</body>
+const myElement = (
+  <table>
+  <tr>
+    <th>Name</th>
+  </tr>
+  <tr>
+    <td>John</td>
+  </tr>
+  <tr>
+    <td>Elsa</td>
+  </tr>
+  </table>
 );
-ReactDOM.render(<p>Hallo</p>, document.getElementById('sandy'));
+
+ReactDOM.render(myElement, document.getElementById('root'));
 }
 
 export default Header
