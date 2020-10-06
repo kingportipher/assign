74bea51b1552adb154d7ca1b87a21c6b47bff77d
unknown <COMSYS@cgl-eng004.comsysghana.com>
2020-10-06 11:46:34 +0000

    First Component commit 1

--- mest/src/App.js
+++ mest/src/App.js
@@ -1,25 +1,14 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import FirstComponent from './FirstComponent'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <p>This is the second</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+function App (){
+  return(
+    <div>
+    <FirstComponent/>
+       <p>Hello World</p>
     </div>
   );
 }
---
+++ mest/src/FirstComponent.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function FirstComponent (){
+  return (
+    <>
+     <h2>This is the FirstComponent Page</h2>
+     <p>For more information visit <a href="https://google.com" target="_blank">Google</a></p>
+    </>
+  );
+}
+
+export default FirstComponent;
